f3b1bb5b191c9a1606ae323843aff898466d78c6
JesseGarcia562 <jessegarcia562@ucla.edu>
2020-08-26 21:59:22 -0700

    Create fig_5_data.sh

---
+++ fig5/fig_5_data.sh
@@ -0,0 +1,5 @@
+wget https://drive.google.com/file/d/1Kf07HUeM02LA84_2hikrlZP_vAceyUNh/view?usp=sharing
+wget https://drive.google.com/file/d/1Gyo7ytWVtpH2jfvndx4ao6YyEAYGuwoq/view?usp=sharing
+wget https://drive.google.com/file/d/1e837PLGfpggX4eyL6Hd1gtePzB_fVj_G/view?usp=sharing
+wget https://drive.google.com/file/d/1nvxqOKxuKjjiWe7l6w9ZSjv8Mo805Ylg/view?usp=sharing
+wget https://drive.google.com/file/d/1XKje4R8BSa1niY0DnLk6BSGtTDWEFliS/view?usp=sharing
