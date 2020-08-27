a0b1c11cd9ba02adb56f4a24b2d59c0d3ca728bb
JesseGarcia562 <jessegarcia562@ucla.edu>
2020-08-26 22:08:27 -0700

    Create fig_7_data.sh

---
+++ fig7/fig_7_data.sh
@@ -0,0 +1,3 @@
+wget https://drive.google.com/file/d/1fIzmxAxAJsj7Vnax9zYq5Uv4ikKLTh5o/view?usp=sharing
+wget https://drive.google.com/file/d/1vzOWdZH-Ew0atiywzQF0jTGNsgfuMq6p/view?usp=sharing
+wget https://drive.google.com/file/d/1b8A_2qh36c2Vf0H0UbpzkYCRrZCNXt4W/view?usp=sharing
