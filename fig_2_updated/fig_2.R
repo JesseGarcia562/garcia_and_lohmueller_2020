db891c3a550d388be492085fab8aab8e4f0eb89a
JesseGarcia562 <jessegarcia562@ucla.edu>
2021-03-30 10:29:12 -0700

    Update and rename fig_3.R to fig_2.R

--- fig_2_updated/fig_2.R
+++ fig_2_updated/fig_2.R
@@ -2,7 +2,7 @@ c("1.5", "", "" ,"6" ,"","","10")
 
 
 ## Recombination of 1e-09
-simulation_df<-read_rds("configuration_gravel_simulations_low_recomb_ac_1_2.rds") %>% mutate_if(is.numeric, replace_na,  0) %>%
+simulation_df <- read_rds("configuration_gravel_simulations_low_recomb_ac_1_2.rds") %>% mutate_if(is.numeric, replace_na,  0) %>%
   mutate(distance=abs(pos_1-pos_2)) %>%
   mutate(variation_type=case_when(
     variation_type == 1 ~ "Nonsynonymous",
@@ -10,21 +10,21 @@ simulation_df<-read_rds("configuration_gravel_simulations_low_recomb_ac_1_2.rds"
   )) %>%
   mutate(variation_type=as.factor(variation_type)) %>% filter(recombination_rate==1e-09 )
 
-long_simulation_df_genotype_config<-gather(simulation_df, 
+long_simulation_df_genotype_config <- gather(simulation_df, 
                                  "0|0,0|0", "0|0,0|1", "1|0,0|0", "0|0,1|0", "0|1,0|0", "1|0,0|1", "1|0,1|0", "0|1,1|0", "0|1,0|1", "1|1,0|0" ,"0|0,1|1" ,"1|1,1|1" , 
                                  value="count", 
                                  key="genotype") 
 
 
-long_simulation_df_genotype_config<-annotate_genotypes(long_simulation_df_genotype_config, type="phased")
+long_simulation_df_genotype_config <- annotate_genotypes(long_simulation_df_genotype_config, type="phased")
 
 
-annotated_genotypes_recomb_1e9<-long_simulation_df_genotype_config
+annotated_genotypes_recomb_1e9 <- long_simulation_df_genotype_config
 
 
 ## Recombination of 1e-08
 
-simulation_df<-read_rds("configuration_gravel_simulations_average_recomb_ac_1_2.rds") %>% mutate_if(is.numeric, replace_na,  0) %>%
+simulation_df <- read_rds("configuration_gravel_simulations_average_recomb_ac_1_2.rds") %>% mutate_if(is.numeric, replace_na,  0) %>%
   mutate(distance=abs(pos_1-pos_2)) %>%
   mutate(variation_type=case_when(
     variation_type == 1 ~ "Nonsynonymous",
@@ -32,16 +32,16 @@ simulation_df<-read_rds("configuration_gravel_simulations_average_recomb_ac_1_2.
   )) %>%
   mutate(variation_type=as.factor(variation_type)) %>% filter(recombination_rate==1e-08 )
 
-long_simulation_df_genotype_config<-gather(simulation_df, 
+long_simulation_df_genotype_config <- gather(simulation_df, 
                                  "0|0,0|0", "0|0,0|1", "1|0,0|0", "0|0,1|0", "0|1,0|0", "1|0,0|1", "1|0,1|0", "0|1,1|0", "0|1,0|1", "1|1,0|0" ,"0|0,1|1" ,"1|1,1|1" , 
                                  value="count", 
                                  key="genotype") 
 
 
-long_simulation_df_genotype_config<-annotate_genotypes(long_simulation_df_genotype_config, type="phased")
+long_simulation_df_genotype_config <- annotate_genotypes(long_simulation_df_genotype_config, type="phased")
 
 
-annotated_genotypes_recomb_1e8<-long_simulation_df_genotype_config
+annotated_genotypes_recomb_1e8 <- long_simulation_df_genotype_config
 
 
 
@@ -50,11 +50,11 @@ annotated_genotypes_recomb_1e8<-long_simulation_df_genotype_config
 ## Simulations
 
 #Recombination 1e-09
-simulations_doubletons_hr_recomb_1e9<-plot_rh_unphased(annotated_genotypes_recomb_1e9, window_size = 1500, plot_type = "line_plot" , allele_count_to_analyze = 2)[[3]]
-simulations_singletons_hr_recomb_1e9<-plot_rh_unphased(annotated_genotypes_recomb_1e9, window_size = 1500, plot_type = "line_plot" , allele_count_to_analyze = 1)[[3]]
+simulations_doubletons_hr_recomb_1e9 <- plot_rh_unphased(annotated_genotypes_recomb_1e9, window_size = 1500, plot_type = "line_plot" , allele_count_to_analyze = 2)[[3]]
+simulations_singletons_hr_recomb_1e9 <- plot_rh_unphased(annotated_genotypes_recomb_1e9, window_size = 1500, plot_type = "line_plot" , allele_count_to_analyze = 1)[[3]]
 #Recombination 1e-08
-simulations_doubletons_hr_recomb_1e8<-plot_rh_unphased(annotated_genotypes_recomb_1e8, window_size = 1500, plot_type = "line_plot" , allele_count_to_analyze = 2)[[3]]
-simulations_singletons_hr_recomb_1e8<-plot_rh_unphased(annotated_genotypes_recomb_1e8, window_size = 1500, plot_type = "line_plot" , allele_count_to_analyze = 1)[[3]]
+simulations_doubletons_hr_recomb_1e8 <- plot_rh_unphased(annotated_genotypes_recomb_1e8, window_size = 1500, plot_type = "line_plot" , allele_count_to_analyze = 2)[[3]]
+simulations_singletons_hr_recomb_1e8 <- plot_rh_unphased(annotated_genotypes_recomb_1e8, window_size = 1500, plot_type = "line_plot" , allele_count_to_analyze = 1)[[3]]
 
 
 
@@ -65,7 +65,7 @@ legend_b <- get_legend(
     theme(legend.position = "bottom")
 )
 
-hr_plots<-plot_grid(
+hr_plots <- plot_grid(
 simulations_singletons_hr_recomb_1e9 + theme(legend.position = "none") + scale_x_discrete(labels=c("1.5", "", "" ,"6" ,"","","10")) + xlab(NULL)+
   theme(axis.title.x=element_blank(),
         axis.text.x=element_blank(),
@@ -86,7 +86,7 @@ plot_grid(hr_plots,legend_b, ncol=1, rel_heights = c(1, .1))
 
 
 
-hr_plots<-plot_grid(
+hr_plots <- plot_grid(
 simulations_singletons_hr_recomb_1e9 + theme(legend.position = "none") + scale_x_discrete(labels=c("1.5", "", "" ,"6" ,"","","10"))  + xlab(NULL)+
   theme(axis.title.x=element_blank(),
         axis.text.x=element_blank(),
