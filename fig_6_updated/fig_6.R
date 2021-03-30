b6e3ce47ec45eb36f4fbef09e35a99a839f2d092
JesseGarcia562 <jessegarcia562@ucla.edu>
2021-03-30 10:40:02 -0700

    Update fig_6.R

--- fig_6_updated/fig_6.R
+++ fig_6_updated/fig_6.R
@@ -5,7 +5,7 @@ library(binr)
 library(mltools)
 library(janitor)
 
-## Colorblind pallate
+## Colorblind palette
 cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
 
