80a13766c1f308cfbf963d975c36843a600d8294
JesseGarcia562 <jessegarcia562@ucla.edu>
2021-03-31 17:48:59 -0700

    Update fig_3a.R

--- fig_3_updated/fig_3a.R
+++ fig_3_updated/fig_3a.R
@@ -6,7 +6,7 @@ library(mltools)
 se <- function(x) sqrt(var(x)/length(x))
 
 
-yri_df_b<-as_tibble(read_rds(file ="../data2/YRI_b_value_breaks_5_physical_breaks_200.rds")) %>%
+yri_df_b<-as_tibble(read_rds(file ="/Users/jessegarcia/Documents/SLiM_ParallelRProject/data2/YRI_b_value_breaks_5_physical_breaks_200.rds")) %>%
   dplyr::rename(D=X6,variation=variation_type, r_square=genetic_distance, genetic_distance=X3, DPrime=X7) %>%
   mutate(Variation=glue("={variation}"), rSquare=r_square) %>% mutate(population="YRI") %>%  mutate(Variation=case_when(
   Variation == "=nonsynonymous_SNV" ~ "Nonsynonymous", 
@@ -29,8 +29,7 @@ yri_plot<-yri_df_b %>%
   scale_x_discrete(labels=c("1st quantile", "2nd quantile", "3rd quantile", "4th quantile", "5th quantile", "6th quantile"))+
   theme(text=element_text(size=22))
 
+mean_dprime_of_matched_pairs <- yri_plot + theme(legend.position = "bottom")
 
-
-
-
-yri_plot + theme(legend.position = "bottom")
+mean_dprime_of_matched_pairs
+ggsave(filename = "../figures/figure_3_mean_dprime_of_matched_pairs.tiff", width=20, height=12)
