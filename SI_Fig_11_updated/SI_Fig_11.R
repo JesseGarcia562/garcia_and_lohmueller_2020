dcf8e8d4d0041b9809ed1945a38ccc181bfc1c75
JesseGarcia562 <jessegarcia562@ucla.edu>
2021-07-11 19:16:18 -0700

    Added Labels to subplots
    Added "A", "B", "C", and "D".

--- SI_Fig_11_updated/SI_Fig_11.R
+++ SI_Fig_11_updated/SI_Fig_11.R
@@ -1,6 +1,66 @@
 ##### HRJ coorelation for r=1e-09 allele count = 2 
-
+library(tidyverse)
+library(cowplot)
 theme_set((theme_bw(base_size=22)))
+
+annotate_genotypes<-function(long_genotype_config, type){
+  
+  
+  if (type == "unphased"){
+  long_genotype_config<-long_genotype_config %>%
+  mutate(words=case_when(
+    genotype == "0/0,0/0" ~ "Homozygous reference (0/0,0/0)", 
+    genotype == "0/0,0/1" ~ "Single heterozygote (0/1,0/0 or 0/0,0/1)", 
+    genotype == "0/1,0/0" ~ "Single heterozygote (0/1,0/0 or 0/0,0/1)",
+    genotype == "0/1,0/1" ~ "Double Heterozygote (0/1,0/1)"
+  ))
+  
+  return(long_genotype_config)
+  }
+  
+    
+  if (type == "phased"){
+  long_genotype_config<-long_genotype_config %>%
+  mutate(words=case_when(
+    genotype == "0|0,0|0" ~ "Homozygous reference (0|0,0|0)", 
+    genotype == "0|0,0|1" ~ "Single heterozygote (0|0,0|1 & 1|0,0|0 & 0|1,0|0 & 0|0,1|0)",
+    genotype == "1|0,0|0" ~ "Single heterozygote (0|0,0|1 & 1|0,0|0 & 0|1,0|0 & 0|0,1|0)",
+    genotype == "0|1,0|0" ~ "Single heterozygote (0|0,0|1 & 1|0,0|0 & 0|1,0|0 & 0|0,1|0)",
+    genotype == "0|0,1|0" ~ "Single heterozygote (0|0,0|1 & 1|0,0|0 & 0|1,0|0 & 0|0,1|0)",
+    genotype == "0|1,0|1" ~ "Double heterozygote (0|1,0|1 & 0|1,1|0 & 1|0,1|0 & 1|0,0|1)",
+    genotype == "0|1,1|0" ~ "Double heterozygote (0|1,0|1 & 0|1,1|0 & 1|0,1|0 & 1|0,0|1)",
+    genotype == "1|0,1|0" ~ "Double heterozygote (0|1,0|1 & 0|1,1|0 & 1|0,1|0 & 1|0,0|1)",
+    genotype == "1|0,0|1" ~ "Double heterozygote (0|1,0|1 & 0|1,1|0 & 1|0,1|0 & 1|0,0|1)"
+    
+  ))
+  
+  return(long_genotype_config)
+  }
+  
+  
+    if (type == "phased_coupling"){
+  long_genotype_config<-long_genotype_config %>%
+  mutate(words=case_when(
+    genotype == "0|0,0|0" ~ "Homozygous reference (0|0,0|0)", 
+    genotype == "0|0,0|1" ~ "Single heterozygote (0|0,0|1 & 1|0,0|0 & 0|1,0|0 & 0|0,1|0)",
+    genotype == "1|0,0|0" ~ "Single heterozygote (0|0,0|1 & 1|0,0|0 & 0|1,0|0 & 0|0,1|0)",
+    genotype == "0|1,0|0" ~ "Single heterozygote (0|0,0|1 & 1|0,0|0 & 0|1,0|0 & 0|0,1|0)",
+    genotype == "0|0,1|0" ~ "Single heterozygote (0|0,0|1 & 1|0,0|0 & 0|1,0|0 & 0|0,1|0)",
+    genotype == "0|1,0|1" ~ "Double heterozygote Coupling (0|1,0|1 & 1|0,1|0)",
+    genotype == "0|1,1|0" ~ "Double heterozygote Repulsion (0|1,1|0 & 1|0,0|1)",
+    genotype == "1|0,1|0" ~ "Double heterozygote Coupling (0|1,0|1 & 1|0,1|0)",
+    genotype == "1|0,0|1" ~ "Double heterozygote Repulsion (0|1,1|0 & 1|0,0|1)"
+    
+  ))
+  
+  return(long_genotype_config)
+  }
+  
+  
+  
+  
+}
+
 
 simulation_df<-read_rds("/Users/jessegarcia/Documents/SLiM_ParallelRProject copy/data2/configuration_gravel_simulations_low_recomb_ac_1_2.rds") %>% mutate_if(is.numeric, replace_na,  0) %>%
   mutate(distance=abs(pos_1-pos_2)) %>%
@@ -148,7 +208,7 @@ sim_plot_2<-bind_cols(hr_j,dprime_simulation_df) %>%
 
 
     prow_1<-plot_grid( sim_plot_1+ theme(legend.position="none"),
-           sim_plot_2 + theme(legend.position = "none") + labs(y=""),
+           sim_plot_2 + theme(legend.position = "none") + labs(y=""),labels=c("B","D"),
            align = 'vh',
            hjust = -1,
            nrow = 1
@@ -302,7 +362,7 @@ sim_plot_4<-bind_cols(hr_j,dprime_simulation_df) %>%
 
 
     prow_2<-plot_grid( sim_plot_3+ theme(legend.position="none"),
-           sim_plot_4 + theme(legend.position = "none") + labs(y=""),
+           sim_plot_4 + theme(legend.position = "none") + labs(y=""), labels=c("A", "C"),
            align = 'vh',
            hjust = -1,
            nrow = 1
@@ -319,7 +379,7 @@ legend <- get_legend(sim_plot_1 + theme(legend.position = "bottom"))
 
 # add the legend to the row we made earlier. Give it one-third of the width
 # of one plot (via rel_widths).
-si_fig_11 <- plot_grid( prow_1,prow_2 , legend,ncol = 1, rel_heights = c(1,1, .1))
+si_fig_11 <- plot_grid( prow_2,prow_1 , legend, ncol = 1, rel_heights = c(1,1, .1))
 si_fig_11
 
 
